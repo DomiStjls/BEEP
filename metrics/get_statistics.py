120a8c0c2881a9f140937047bc0e3449a34990ad
Учитель <@domain.impulse35.ru>
2025-04-24 15:58:45 +0300

    final version 2

--- metrics/get_statistics.py
+++ metrics/get_statistics.py
@@ -2,11 +2,14 @@ from onnx_model_copy import predict, size
 from PIL import Image
 
 PATH = "test"
-cnt = 100
+cnt = 123
 
 
 inputsize = size
 sumiou = 0
+sumf1 = 0
+summap50 = 0
+cntmap50 = 0
 fail = 0
 for i in range(cnt):
     try:
@@ -31,13 +34,32 @@ for i in range(cnt):
         union = sum([(y_pred[i] or y_true[i]) for i in range(len(y_pred))])
         if union == 0:
             iou = (1.0 if inter == 0 else 0.0)
-            sumiou += iou
         else:
             iou = inter / union
-            sumiou += iou
-        print(i, inter, union, iou, sumiou / (i + 1 - fail))
+        sumiou += iou
+        if iou > 0.5:
+            summap50 += iou
+            cntmap50 += 1
+        tp = sum([(y_pred[i] and y_true[i]) for i in range(len(y_pred))])
+        tn = sum([(not y_pred[i] and not y_true[i]) for i in range(len(y_pred))])
+        fn = sum([(not y_pred[i] and y_true[i]) for i in range(len(y_pred))])
+        fp = sum([(y_pred[i] and not y_true[i]) for i in range(len(y_pred))])
+        precision = tp / (tp + fp)
+        recall = tp / (tp + fn)
+        if precision + recall == 0:
+            f1score = 0
+        else:
+            f1score = 2 * precision * recall / (precision + recall)
+        sumf1 += f1score
+        n = i + 1 - fail
+        print(f"{i}: {f1score:.5f} {iou:.5f}")
+        # print(f"f1 = {(sumf1 / n):.5f}, iou = {(sumiou / n):.5f}, map50 = {(summap50 / cntmap50):.5f}")
     except Exception as e:
         fail += 1
         if 'Keyboard' in str(e):
             break
         print(e)
+
+n = cnt - fail
+print("Final:")
+print(f"F1 score: {sumf1 / n}, IOU: {sumiou / n}, mAP50: {(summap50 / cntmap50) if cntmap50 != 0 else -1}")
